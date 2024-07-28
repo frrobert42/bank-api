67e0b0b7d65ce3d069e849eeb55efd513630ccbb
Fred <robert.frederic7@gmail.com>
2024-07-28 09:10:08 +0200

    redirect

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,7 @@ export default function Header(props) {
 
     const handleLogout = () => {
         dispatch(logoutUser());
-        navigate('/login');
+        navigate('/');
     };
 
     return (
